23787e787f07460d886af736eb6e14f1af5a5b25
Dillip Kudithipoodi <dxk5326@mavs.uta.edu>
2025-11-10 15:24:43 -0500

    Fix GitHub Actions folder name

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Azure App Service using Flaskkkkkk!"
+    return "Hello from Azure App Service using Flaskkkkkkkkkkk!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
